64c829b764d58668be12b555baa0a4d77125df26
Maxime DELRIEU <mdelrieu@icrc.org>
2017-11-22 17:00:59 +0100

    adding Angular

--- whosyourdaddytheband/wyd/admin.py
+++ whosyourdaddytheband/wyd/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Concert, Role, Member, Picture, Record, Song, Video
+
+admin.site.register(Concert)
+admin.site.register(Role)
+admin.site.register(Member)
+admin.site.register(Picture)
+admin.site.register(Record)
+admin.site.register(Song)
+admin.site.register(Video)
--- whosyourdaddytheband/wyd/models.py
+++ whosyourdaddytheband/wyd/models.py
@@ -1,24 +1,43 @@
 from django.db import models
 
 
-class Instrument(models.Model):
+class Role(models.Model):
     name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class GearType(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class Gear(models.Model):
+    type = models.ForeignKey(GearType, on_delete=models.SET_NULL)
+    member = models.ForeignKey(Member, on_delete=models.CAS)
 
 
 class Member(models.Model):
-    firstname = models.CharField(max_length=128)
-    lastname = models.CharField(max_length=128)
+    firstname = models.CharField(max_length=256)
+    lastname = models.CharField(max_length=256)
+    pseudo = models.CharField(max_length=64, blank=True)
     birth_date = models.DateField()
-    instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
+    instrument = models.ForeignKey(Role, on_delete=models.SET_NULL)
     avatar = models.ImageField()
 
     member_from = models.DateField()
-    member_until = models.DateField()
+    member_until = models.DateField(blank=True)
+
+    def __str__(self):
+        return self.pseudo or ("%s %s" % (self.firstname, self.lastname, ))
 
 
 class Song(models.Model):
     title = models.CharField(max_length=1024)
+    authors = models.ManyToManyField(Member)
+    compositors = models.ManyToManyField(Member)
     duration = models.DurationField()
+    lyrics = models.TextField()
 
 
 class Concert(models.Model):
@@ -30,7 +49,7 @@ class Concert(models.Model):
 
 class Picture(models.Model):
     title = models.CharField(max_length=1024)
-    avatar = models.ImageField()
+    image = models.ImageField()
     members = models.ManyToManyField(Member)
 
 
@@ -42,3 +61,4 @@ class Video(models.Model):
 class Record(models.Model):
     title = models.CharField(max_length=1024)
     content = models.ManyToManyField(Song)
+    artwork = models.ImageField(blank=True)
